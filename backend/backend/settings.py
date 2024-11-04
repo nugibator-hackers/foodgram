64ac05987b8a8aafa053b03519748b501d992d8d
Nikolai <kolya.kurenkov.1999@gmail.com>
2024-11-04 15:23:53 +0300

    а

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,14 +72,6 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 AUTH_USER_MODEL = 'users.User'
 
 AUTH_PASSWORD_VALIDATORS = [
